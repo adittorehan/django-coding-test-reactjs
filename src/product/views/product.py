f7bde580ee3b623cc877d45c76a00d73f4798e55
Aditto Rehan <adittorehanshaju@gmail.com>
2023-05-20 11:20:04 -0600

    Add product filter feature by product title, product variant, price range and date

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,8 +1,11 @@
+from datetime import date, datetime
+
+from django.db.models import Min, Max
 from django.views import generic
 
 from django.views.generic import ListView
 
-from product.models import Variant, Product
+from product.models import Variant, Product, ProductVariantPrice
 
 
 class CreateProductView(generic.TemplateView):
@@ -21,6 +24,35 @@ class ProductListView(ListView):
     model = Product
     paginate_by = 2
 
+    def get_queryset(self):
+        title = self.request.GET.get('title') or ""
+        variant = self.request.GET.get('variant') or ""
+        price_from = self.request.GET.get('price_from') or \
+                     ProductVariantPrice.objects.aggregate(Min('price'))['price__min']
+        price_to = self.request.GET.get('price_to') or ProductVariantPrice.objects.aggregate(Max('price'))['price__max']
+        date = self.request.GET.get('date')
+        if date:
+            start_date = datetime.strptime(date, "%Y-%m-%d")
+        else:
+            start_date = Product.objects.aggregate(Min('created_at'))['created_at__min']
+        end_date = datetime.today()
+        return Product.objects.filter(
+            title__icontains=title,
+            productvariantprice__price__range=(price_from, price_to),
+            productvariant__variant_title__contains=variant,
+            created_at__range=(start_date, end_date)
+        ).distinct()
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        variants = Variant.objects.filter(active=True).order_by('id')
+        variant_list = []
+        for variant in variants:
+            variant_list.append({
+                'id': variant.id,
+                'title': variant.title,
+                'product_variants': list(variant.productvariant_set.all().values('variant_title').distinct())
+
+            })
+        context['variants'] = list(variant_list)
         return context
