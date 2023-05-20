5848027678ccba9f90a4504b675a9bcc31e57466
Aditto Rehan <adittorehanshaju@gmail.com>
2023-05-20 06:39:27 -0600

    Make list view of products

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,6 +1,8 @@
 from django.views import generic
 
-from product.models import Variant
+from django.views.generic import ListView
+
+from product.models import Variant, Product
 
 
 class CreateProductView(generic.TemplateView):
@@ -11,4 +13,14 @@ class CreateProductView(generic.TemplateView):
         variants = Variant.objects.filter(active=True).values('id', 'title')
         context['product'] = True
         context['variants'] = list(variants.all())
+        return context
+
+
+class ProductListView(ListView):
+    template_name = 'products/list.html'
+    model = Product
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
         return context
